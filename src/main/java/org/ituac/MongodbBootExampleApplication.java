e061074fbe8cbd874e1da27cd8ed2b2accac5fe6
ituac <ituac@qq.com>
2019-06-11 11:12:05 +0800

    init

---
+++ src/main/java/org/ituac/MongodbBootExampleApplication.java
@@ -0,0 +1,13 @@
+package org.ituac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbBootExampleApplication.class, args);
+	}
+
+}
